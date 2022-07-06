725cbea4e5c4df1dd186b7566ec0f3916708d671
Micah Sterling <msterling89@gmail.com>
2022-07-06 16:08:36 -0500

    add seed data/ slugify to org model

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,11 @@
 class Organization < ApplicationRecord
   has_many :users
   has_many :shifts
+
+  before_create :slugify
+  
+  def slugify
+    self.slug = name.parameterize
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,62 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+# organization = Organization.create([
+#   {
+#     name: "Bobs Burgers",
+#     hourly_rate: 10
+#   },
+#   {
+#     name: "Moes Tavern",
+#     hourly_rate: 15
+#   },
+#   {
+#     name: "Sallys Sandwiches",
+#     hourly_rate: 20
+#   },
+# ])
+
+# user = User.create([
+#   {
+#     name: "Jane Brown",
+#     email_address: "Janeb@email.com",
+#     password: "password1",
+#     organization_id: 1
+#   },
+#   {
+#     name: "John Smith",
+#     email_address: "Johns@email.com",
+#     password: "password2",
+#     organization_id: 2
+#   },
+#   {
+#     name: "Ellen Jones",
+#     email_address: "EllenJ@email.com",
+#     password: "password3",
+#     organization_id: 3
+#   },
+# ])
+
+# shift = Shift.create([
+#   {
+#     user_id: 1,
+#     organization_id: 1,
+#     start: DateTime.new(2022,7,1,8),
+#     end: DateTime.new(2022,7,1,12),
+#   },
+#   {
+#     user_id: 2,
+#     organization_id: 1,
+#     start: DateTime.new(2022,7,2,8),
+#     end: DateTime.new(2022,7,2,12),
+#   },
+#   {
+#     user_id: 3,
+#     organization_id: 1,
+#     start: DateTime.new(2022,7,3,8),
+#     end: DateTime.new(2022,7,3,12),
+#   },
+# ])
+
+p "data imported"
