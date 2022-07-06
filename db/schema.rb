3e37a2761449d7aa749e063af8296682479a54b8
Micah Sterling <msterling89@gmail.com>
2022-07-06 14:22:37 -0500

    add shift model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_06_191636) do
+ActiveRecord::Schema.define(version: 2022_07_06_192107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,18 @@ ActiveRecord::Schema.define(version: 2022_07_06_191636) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "shifts", force: :cascade do |t|
+    t.datetime "start"
+    t.datetime "end"
+    t.float "break_length"
+    t.bigint "user_id", null: false
+    t.bigint "organization_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["organization_id"], name: "index_shifts_on_organization_id"
+    t.index ["user_id"], name: "index_shifts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email_address"
@@ -33,5 +45,7 @@ ActiveRecord::Schema.define(version: 2022_07_06_191636) do
     t.index ["organization_id"], name: "index_users_on_organization_id"
   end
 
+  add_foreign_key "shifts", "organizations"
+  add_foreign_key "shifts", "users"
   add_foreign_key "users", "organizations"
 end
