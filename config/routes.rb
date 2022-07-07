ec2a763cc1397cdd89df0b6b8fd813a722ac3b02
Micah Sterling <msterling89@gmail.com>
2022-07-07 17:39:37 -0500

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :organization, param: :slug
+      resources :organizations, param: :slug
     end
   end
 
