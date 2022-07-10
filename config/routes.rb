9d8d43a232eed58ed2a3c38ce2d680e63538dd15
Micah Sterling <msterling89@gmail.com>
2022-07-10 09:28:52 -0500

    add shift index route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :organizations, param: :slug
+      # resources :shifts
+      get "/shifts" => "shifts#index"
     end
   end
 
