13fe7838d80373fba06997fd79bd2121dc52a92f
Micah Sterling <msterling89@gmail.com>
2022-07-14 16:04:43 -0500

    add raills authentication

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :organizations, param: :slug
-      # resources :shifts
-      get "/shifts" => "shifts#index"
+      resources :shifts
+
+      
+      post "/users" => "users#create"
+      post "/sessions" => "sessions#create"
     end
   end
 
