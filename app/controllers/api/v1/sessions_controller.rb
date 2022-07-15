ac4e0825c7f75b490c134acb76944726cbaf41e2
Micah Sterling <msterling89@gmail.com>
2022-07-15 14:17:40 -0500

    update sessions naming

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   def create
-    user = User.find_by(email_address: params[:email_address])
+    user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       jwt = JWT.encode(
         {
@@ -11,7 +12,7 @@ class Api::V1::SessionsController < ApplicationController
         Rails.application.credentials.fetch(:secret_key_base), # the secret key
         "HS256" # the encryption algorithm
       )
-      render json: { jwt: jwt, email: user.email_address, user_id: user.id }, status: :created
+      render json: { jwt: jwt, email: user.email, user_id: user.id }, status: :created
     else
       render json: {}, status: :unauthorized
     end
