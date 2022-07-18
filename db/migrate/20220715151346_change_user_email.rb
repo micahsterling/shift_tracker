7581d9857659f186d5896e7ec9927333cad0f777
Micah Sterling <msterling89@gmail.com>
2022-07-18 17:11:22 -0500

    update schema

---
+++ db/migrate/20220715151346_change_user_email.rb
@@ -0,0 +1,5 @@
+class ChangeUserEmail < ActiveRecord::Migration[6.1]
+  def change
+    rename_column :users, :email_address, :email
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_14_204635) do
+ActiveRecord::Schema.define(version: 2022_07_15_151346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2022_07_14_204635) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email_address"
+    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
