bf6f679f05f11cf6315984f2e025ef98336299fa
Micah Sterling <msterling89@gmail.com>
2022-07-21 16:34:09 -0500

    add create to members

--- app/controllers/api/v1/memberships_controller.rb
+++ app/controllers/api/v1/memberships_controller.rb
@@ -3,6 +3,18 @@ class Api::V1::MembershipsController < ApplicationController
   def index
     @memberships = Membership.where(user_id: params[:user_id]) 
 
-    render 'index.json.jb'
+    render 'index'
+  end
+
+  def create 
+    @membership = Membership.new(
+      user_id: params[:user_id],
+      organization_id: params[:organization_id]
+    )
+    if @membership.save
+      render 'show.json.jb'
+    else
+      render json: {error: @membership.errors.messages}, status: 422
+    end
   end
 end
