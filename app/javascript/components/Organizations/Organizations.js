1d4c74f6cd485811c8bdd2f68e4899bbbedae7c3
Micah Sterling <msterling89@gmail.com>
2022-07-26 17:00:06 -0500

    import header/ state/ logout

--- app/javascript/components/Organizations/Organizations.js
+++ app/javascript/components/Organizations/Organizations.js
@@ -1,25 +1,24 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "../../api/axios";
 import AuthContext from "../../context/AuthProvider";
-import { Membership, Organization, getOrgId } from "./OrgMemList";
-import { Link, useNavigate, useLocation } from "react-router-dom";
+import Header from "../Header/Header";
+import { Membership, Organization } from "./OrgMemList";
+import { Body, Form, List, ListTitle, TextWrap } from "./OrgElements";
 
 const Organizations = () => {
-  const { logout, currentUser } = useContext(AuthContext);
+  const {
+    currentUser,
+    organizations,
+    setOrganizations,
+    memberships,
+    setMemberships,
+    setShow,
+  } = useContext(AuthContext);
 
-  console.log("current user", currentUser);
-  // const currentUser = {
-  //   name: localStorage.getItem("name"),
-  //   id: localStorage.getItem("user_id"),
-  // };
-  const [organizations, setOrganizations] = useState([]);
-  const [memberships, setMemberships] = useState([]);
-  const [orgFilter, setOrgFilter] = useState([]);
-  const navigate = useNavigate();
-  const location = useLocation();
-  const from = location.state?.from?.pathname || "/";
+  const [formData, setFormData] = useState({});
 
   useEffect(() => {
+    setShow(false);
     // Get user memberships from api and update state
     axios
       .get("memberships.json", {
@@ -37,8 +36,8 @@ const Organizations = () => {
       })
       .catch((resp) => console.log("catch", resp));
   }, [setMemberships, setOrganizations]);
-  console.log("mem", memberships);
-  console.log("org", organizations);
+  // console.log("mem", memberships);
+  // console.log("org", organizations);
 
   const memberList = memberships.map((item) => {
     return (
@@ -46,15 +45,8 @@ const Organizations = () => {
         name={item.name}
         slug={item.slug}
         org_id={item.organization_id}
+        id={item.id}
       />
-      // <div className="card">
-      //   <div className="org name">{item.name}</div>
-      //   <button className="edit">Edit</button>
-      //   {/* <button className="join">Join</button> */}
-      //   <div className="link">
-      //     <Link to={`/organizations/${item.slug}`}>Shifts</Link>
-      //   </div>
-      // </div>
     );
   });
 
@@ -62,37 +54,73 @@ const Organizations = () => {
     (org) => !memberships.find((mem) => org.name === mem.name)
   );
 
-  console.log("diff", diff);
+  // console.log("diff", diff);
 
   const orgList = diff.map((item) => {
-    return <Organization name={item.name} />;
+    return <Organization name={item.name} org_id={item.id} />;
   });
 
-  // function logout() {
-  //   localStorage.clear();
-  //   console.log("logged out");
-  //   navigate(from, { replace: true });
-  // }
+  const handleFormChange = (e) => {
+    e.preventDefault();
+
+    setFormData(
+      Object.assign({}, formData, { [e.target.name]: e.target.value })
+    );
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("organizations", {
+        user_id: currentUser.id,
+        name: formData.name,
+        hourly_rate: formData.hourly_rate,
+      })
+      .then((resp) => {
+        setMemberships([formData, ...memberships]);
+      })
+      .catch((err) => {
+        if (err.response?.status === 422) {
+          alert("Name has already been taken");
+        }
+      });
+    setFormData("");
+  };
 
   return (
-    <div>
-      <p>Logged in as {currentUser.name}</p>
-      <button onClick={logout}>Log Out</button>
-      <p>You aren't a member of an organization.</p>
-      <p>Join and existing one or create a new one.</p>
-      <h1>My Organizations</h1>
-      <ul>{memberList}</ul>
-      <h1>Organizations</h1>
-      <ul>{orgList}</ul>
-      <h1>Create Organization</h1>
-      <form action="" method="post">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" />
-        <label for="hourly rate">Hourly Rate: $</label>
-        <input type="text" id="hourly rate" name="hourly rate" />
-        <input type="submit" value="Create and Join" />
-      </form>
-    </div>
+    <>
+      <Header />
+      <Body>
+        <TextWrap>
+          <p>You aren't a member of an organization.</p>
+          <p>Join and existing one or create a new one.</p>
+        </TextWrap>
+        <ListTitle>My Organizations</ListTitle>
+        <List>{memberList}</List>
+        <ListTitle>Organizations</ListTitle>
+        <List>{orgList}</List>
+        <ListTitle>Create Organization</ListTitle>
+        <Form onSubmit={handleSubmit}>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            required
+            value={formData.name || ""}
+            onChange={handleFormChange}
+          />
+          <label>Hourly Rate: $</label>
+          <input
+            type="number"
+            name="hourly_rate"
+            required="required"
+            value={formData.hourly_rate || ""}
+            onChange={handleFormChange}
+          />
+          <input type="submit" value="Create and Join" />
+        </Form>
+      </Body>
+    </>
   );
 };
 
