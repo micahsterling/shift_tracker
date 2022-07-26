5d6e71996aef48e4f4ae0b6df8aa51ffea7fa1d5
Micah Sterling <msterling89@gmail.com>
2022-07-26 16:51:53 -0500

    update referance

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Routes, Route } from "react-router-dom";
 import Shifts from "./components/Shifts/Shifts";
 import Login from "./components/User/Login";
 import Organizations from "./components/Organizations/Organizations";
 import Signup from "./components/User/SignUp";
-import useAuth from "./hooks/UseAuth";
 import EditOrg from "./components/Organizations/OrgEdit";
+import AuthContext from "./context/AuthProvider";
 
 const App = () => {
-  const { currentUser } = useAuth();
+  const { currentUser } = useContext(AuthContext);
 
   if (currentUser.jwt === null) {
     return (
