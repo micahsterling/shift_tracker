887216a3964454696b653b2ee4243533de6da1ff
Micah Sterling <msterling89@gmail.com>
2022-08-03 12:27:51 -0500

    update config for heroku

--- app/javascript/api/axios.js
+++ app/javascript/api/axios.js
@@ -1,15 +1,10 @@
 import axios from "axios";
 
-// development url
-// export default axios.create({
-//   baseURL: "http://localhost:3000/api/v1",
-//   headers: { "Content-Type": "application/json" },
-//   withCredentials: true,
-// });
-
-// production baseURL
 export default axios.create({
-  baseURL: "https://tanda-web-app.herokuapp.com/api/v1",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://tanda-web-app.herokuapp.com/api/v1"
+      : "http://localhost:3000/api/v1",
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
