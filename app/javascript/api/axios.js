b3f4d6cb6654e5ed7c0f2b9ec2b98cac1b5c84bb
Micah Sterling <msterling89@gmail.com>
2022-08-02 19:27:07 -0500

    update axios to production

--- app/javascript/api/axios.js
+++ app/javascript/api/axios.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
+// development url
+// export default axios.create({
+//   baseURL: "http://localhost:3000/api/v1",
+//   headers: { "Content-Type": "application/json" },
+//   withCredentials: true,
+// });
+
+// production baseURL
 export default axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://tanda-web-app.herokuapp.com/",
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
