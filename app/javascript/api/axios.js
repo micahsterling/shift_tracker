dd625df824a10bedca002417b656654f4e7c733e
Micah Sterling <msterling89@gmail.com>
2022-11-07 17:52:27 -0600

    update axios test

--- app/javascript/api/axios.js
+++ app/javascript/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://tanda-web-app.herokuapp.com/api/v1"
+      ? "https://shift-tracker.up.railway.app/api/v1" || 'https://shift-tracker.up.railway.app/'
       : "http://localhost:3000/api/v1",
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
