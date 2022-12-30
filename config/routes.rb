4346b688017645efb3840d31a03e631bb07e7d49
Micah Sterling <msterling89@gmail.com>
2022-12-30 12:13:17 -0600

    add fix for update changes

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,8 @@ Rails.application.routes.draw do
       resources :shifts
       resources :memberships
 
-      
       post "/users" => "users#create"
       post "/sessions" => "sessions#create"
     end
   end
-
-  get '*path', to: "pages#index", via: :all
 end
